8fd4547b867f1af5d3afbd73d58b86559622e5ca
SergeyKinyak <kinyaksereja@gmail.com>
2025-05-14 16:29:37 +0300

    rocket

--- main.py
+++ main.py
@@ -14,6 +14,13 @@ fighter_width, fighter_height = fighter_image.get_size()
 fighter_x, fighter_y = screen_width / 2 - fighter_width / 2, screen_height - fighter_height
 fighter_is_moving_left, fighter_is_moving_right = False, False
 
+BALL_STEP=0.5
+ball_image=pygame.image.load('images/rocket.png ')
+ball_width,ball_height=ball_image.get_size()
+ball_x,ball_y=fighter_x+fighter_width/2-ball_width/2,fighter_y-ball_height
+ball_was_fired=False
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -24,7 +31,10 @@ while True:
 
             if event.key == pygame.K_RIGHT:
                 fighter_is_moving_right = True
-
+            if event.key==pygame.K_SPACE:
+                ball_was_fired=True
+                ball_x=fighter_x + fighter_width / 2 - ball_width / 2
+                ball_y=fighter_y - ball_height
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 fighter_is_moving_left = False
@@ -35,8 +45,19 @@ while True:
         fighter_x -= FIGHTER_STEP
     if fighter_is_moving_right and fighter_x <= screen_width - fighter_width - FIGHTER_STEP:
         fighter_x += FIGHTER_STEP
+    if ball_was_fired and ball_y+ball_height <0:
+        ball_was_fired = False
+
+    if ball_was_fired:
+        ball_y -= BALL_STEP
+
+
 
     screen.fill(screen_fill_color)
     screen.blit(fighter_image, (fighter_x, fighter_y))
+
+    if ball_was_fired:
+        screen.blit(ball_image, (ball_x, ball_y))
+
 
     pygame.display.update()
